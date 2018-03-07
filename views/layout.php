3b5b910e90b5172859f657e30129779e814a6587
Aaron Parecki <aaron@parecki.com>
2018-03-07 09:04:58 -0800

    full-bleed on narrow screens

--- views/layout.php
+++ views/layout.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="/assets/styles.css" rel="stylesheet" type="text/css">
   <link href="/assets/bulma-0.6.2/css/bulma.css" rel="stylesheet">
+  <link href="/assets/styles.css" rel="stylesheet" type="text/css">
   <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
   <script src="/assets/jquery-3.3.1.min.js"></script>
 
