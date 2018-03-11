922f6217b3cfd128ad647e131e3a2a9bde07c5ed
Aaron Parecki <aaron@parecki.com>
2018-03-11 11:49:06 -0700

    move "like-of" rendering into a template

--- views/timeline.php
+++ views/timeline.php
@@ -31,30 +31,7 @@
           <? if(isset($entry['like-of'])): ?>
 
             <? /* TODO: if expanded content of the like-of is available, render that post instead */ ?>
-            <div class="content">
-              <span class="author">
-                <? if(!empty($entry['author']['photo'])): ?>
-                  <img src="<?= e($entry['author']['photo']) ?>">
-                <? endif ?>
-                <? if(!empty($entry['author']['url'])): ?>
-                  <a href="<?= e($entry['author']['url']) ?>">
-                    <?= e($entry['author']['name'] ?? \p3k\url\display_url($entry['author']['url'])) ?>
-                  </a>
-                <? else: ?>
-                  someone
-                <? endif ?>
-              </span>
-              liked
-              <? if(count($entry['like-of']) == 1): ?>
-                <a href="<?= e($entry['like-of'][0]) ?>">a post
-                on <?= parse_url($entry['like-of'][0], PHP_URL_HOST) ?></a>
-              <? else: ?>
-                <? foreach($entry['like-of'] as $i=>$l): ?>
-                  <?= $i == count($entry['like-of']) - 1 ? 'and' : '' ?>
-                  <a href="<?= e($l) ?>"><?= e(\p3k\url\display_url($l)) ?></a>
-                <? endforeach ?>
-              <? endif ?>
-            </div>
+            <?= $this->insert('timeline/compact-like', ['entry' => $entry]) ?>
             <?= $this->insert('timeline/actions', ['entry' => $entry, 'responses_enabled' => $responses_enabled]) ?>
             <?= $this->insert('timeline/meta', ['entry' => $entry]) ?>
 
