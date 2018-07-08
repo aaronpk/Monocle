47dc533cb490af11d0eb08daeaa3d674df10416d
Aaron Parecki <aaron@parecki.com>
2018-07-08 06:50:39 -0700

    Display all dates in the browser's locale and timezone

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,5 +1,12 @@
 $(function(){
 
+  // Display all dates in the browser's locale and timezone
+  // Stashes the original display date in the "title" element so you can still see it on hover
+  $("time").map(function(i, el){
+    $(el).attr("title", $(el).text().trim());
+    $(el).text((new Date($(el).attr("datetime"))).toLocaleString());
+  });
+
   /* Global JS to enable the close button on modals */
 
   function closeActiveModal() {
