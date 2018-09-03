9e762255e12a4080d9bba1bc363d15bdc53dc326
Stephen Rushe <srushe@users.noreply.github.com>
2018-09-03 20:45:12 +0100

    Expand the click areas in the channel list

--- views/components/channel-list.php
+++ views/components/channel-list.php
@@ -1,8 +1,10 @@
     <?php foreach($_SESSION['channels'] as $channel): ?>
       <li data-channel-uid="<?= e($channel['uid']) ?>">
-        <a href="/channel/<?= e(urlencode($channel['uid'])) ?>"><?= e($channel['name']) ?></a>
-        <?php if(isset($channel['unread'])): ?>
-          <span class="tag is-info <?= is_bool($channel['unread']) ? 'is-dot' : 'is-rounded' ?> <?= isset($channel['unread']) && $channel['unread'] > 0 ? '' : 'is-hidden' ?>"><?= is_bool($channel['unread']) ? '' : $channel['unread'] ?></span>
-        <?php endif ?>
+        <a href="/channel/<?= e(urlencode($channel['uid'])) ?>">
+          <?= e($channel['name']) ?>
+          <?php if(isset($channel['unread'])): ?>
+            <span class="tag is-info <?= is_bool($channel['unread']) ? 'is-dot' : 'is-rounded' ?> <?= isset($channel['unread']) && $channel['unread'] > 0 ? '' : 'is-hidden' ?>"><?= is_bool($channel['unread']) ? '' : $channel['unread'] ?></span>
+          <?php endif ?>
+        </a>
       </li>
     <?php endforeach; ?>
