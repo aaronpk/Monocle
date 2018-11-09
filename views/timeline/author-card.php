6e9bdf8aca79e513fb6ad16abcba9c6a3bd49e48
Aaron Parecki <aaron@parecki.com>
2018-11-09 08:38:05 -0800

    fix logic around displaying parts of author h-cards

--- views/timeline/author-card.php
+++ views/timeline/author-card.php
@@ -1,20 +1,17 @@
           <div class="author u-author h-card">
-            <? if(!empty($entry['author']['name']) && !empty($entry['author']['photo']) && !empty($entry['author']['url'])): ?>
+            <? if(!empty($entry['author']['photo'])): ?>
               <img src="<?= e($entry['author']['photo']) ?>" class="u-photo">
+            <? endif ?>
+            <? if(!empty($entry['author']['name']) || !empty($entry['author']['url'])): ?>
               <div class="author-name">
-                <a href="<?= e($entry['author']['url']) ?>" class="name p-name"><?= e($entry['author']['name']) ?></a>
-                <a href="<?= e($entry['author']['url']) ?>" class="url u-url"><?= e(\p3k\url\display_url($entry['author']['url'])) ?></a>
-              </div>
-            <? elseif(!empty($entry['author']['url'])): ?>
-              <div class="author-name">
-                <a href="<?= e($entry['author']['url']) ?>" class="name p-name"><?= e($entry['author']['name']) ?></a>
-                <a href="<?= e($entry['author']['url']) ?>" class="url u-url"><?= e(\p3k\url\display_url($entry['author']['url'])) ?></a>
-              </div>
-            <? elseif(!empty($entry['author']['name'])): ?>
-              <div class="author-name">
+                <? if(!empty($entry['author']['url'])): ?>
+                  <? if(!empty($entry['author']['name'])): ?>
+                    <a href="<?= e($entry['author']['url']) ?>" class="name p-name"><?= e($entry['author']['name']) ?></a>
+                  <? endif ?>
+                  <a href="<?= e($entry['author']['url']) ?>" class="url u-url"><?= e(\p3k\url\display_url($entry['author']['url'])) ?></a>
+                <? elseif(!empty($entry['author']['name'])): ?>
                   <span class="name p-name"><?= e($entry['author']['name']) ?></span>
+                <? endif ?>
               </div>
-            <? else: ?>
-
             <? endif ?>
           </div>
