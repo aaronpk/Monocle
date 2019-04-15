f63624001dfe87489ae6d83b48c0957e44d15fde
Aaron Parecki <aaron@parecki.com>
2019-04-15 12:46:30 -0700

    don't link to source unless source is set

--- views/timeline/author-card.php
+++ views/timeline/author-card.php
@@ -1,12 +1,22 @@
           <div class="author u-author h-card">
             <? if(!empty($entry['author']['photo'])): ?>
-              <a href="/channel/<?= e($channel['uid']) ?>/<?= e($entry['_source']) ?>"><img src="<?= e($entry['author']['photo']) ?>" class="u-photo"></a>
+              <? if(isset($entry['_source'])): ?>
+                <a href="/channel/<?= e($channel['uid']) ?>/<?= e($entry['_source']) ?>">
+                  <img src="<?= e($entry['author']['photo']) ?>" class="u-photo">
+                </a>
+              <? else: ?>
+                <img src="<?= e($entry['author']['photo']) ?>" class="u-photo">
+              <? endif ?>
             <? endif ?>
             <? if(!empty($entry['author']['name']) || !empty($entry['author']['url'])): ?>
               <div class="author-name">
                 <? if(!empty($entry['author']['url'])): ?>
                   <? if(!empty($entry['author']['name'])): ?>
-                    <a href="/channel/<?= e($channel['uid']) ?>/<?= e($entry['_source']) ?>" class="name p-name"><?= e($entry['author']['name']) ?></a>
+                    <? if(isset($entry['_source'])): ?>
+                      <a href="/channel/<?= e($channel['uid']) ?>/<?= e($entry['_source']) ?>" class="name p-name"><?= e($entry['author']['name']) ?></a>
+                    <? else: ?>
+                      <a href="<?= e($entry['author']['url']) ?>" class="name p-name"><?= e($entry['author']['name']) ?></a>
+                    <? endif ?>
                   <? endif ?>
                   <a href="<?= e($entry['author']['url']) ?>" class="url u-url"><?= e(\p3k\url\display_url($entry['author']['url'])) ?></a>
                 <? elseif(!empty($entry['author']['name'])): ?>
