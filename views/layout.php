61f248186e0062bca41ea2ca3dce1b3cf6905617
Aaron Parecki <aaron@parecki.com>
2019-06-19 18:02:19 +0100

    update to bulma 0.7.5

--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="/assets/bulma-0.6.2/css/bulma.css" rel="stylesheet">
+  <link href="/assets/bulma-0.7.5/css/bulma.css" rel="stylesheet">
   <link rel="stylesheet" href="/assets/featherlight-1.5.0/featherlight.min.css">
 
   <link href="/assets/styles.css" rel="stylesheet" type="text/css">
