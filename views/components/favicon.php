aca586ef567830f57cdbede2945be713d48a8c7e
Aaron Parecki <aaron@parecki.com>
2019-10-02 13:52:38 +0200

    move favicon to root

--- views/components/favicon.php
+++ views/components/favicon.php
@@ -3,7 +3,7 @@
       <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
       <link rel="manifest" href="/icons/manifest.json">
       <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
-      <link rel="shortcut icon" href="/icons/favicon.ico">
+      <link rel="shortcut icon" href="/favicon.ico">
       <meta name="msapplication-TileColor" content="#da532c">
       <meta name="msapplication-config" content="/icons/browserconfig.xml">
       <meta name="theme-color" content="#ffffff">
